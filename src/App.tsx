73cd55e8d409149e83a51b62ed105b4ff90d46b9
Cobi Reeves <cobi.reeves@gmail.com>
2023-02-28 14:45:01 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Experience from "./components/Experience"
+import Intro from "./components/Intro"
+import Projects from "./components/Projects"
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-white text-center">
+        <Intro />
+        <Projects />
+        <Experience />
+    </div>
+  )
+}
+
+export default App
